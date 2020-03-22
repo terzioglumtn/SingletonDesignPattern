443304016ad5d9df44d9e7dd507b6580938faac9
terzioglumtn <terzioglumtn@hotmail.com>
2020-03-22 23:21:41 +0300

    Add project files.

---
+++ WindowsFormsApp_Singleton_Implement/Form1.Designer.cs
@@ -0,0 +1,63 @@
+﻿namespace WindowsFormsApp_Singleton_Implement
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView
+            // 
+            this.dataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView.Location = new System.Drawing.Point(12, 12);
+            this.dataGridView.Name = "dataGridView";
+            this.dataGridView.RowTemplate.Height = 24;
+            this.dataGridView.Size = new System.Drawing.Size(776, 426);
+            this.dataGridView.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dataGridView);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView;
+    }
+}
+
---
+++ WindowsFormsApp_Singleton_Implement/FxFunction.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp_Singleton_Implement
+{
+    public class FxFunction
+    {
+        private static FxFunction _instance;
+
+        //Ctor
+        private FxFunction()
+        {
+
+        }
+
+        public static FxFunction GetInstance()
+        {
+            if (_instance == null)
+            {
+                _instance = new FxFunction();
+            }
+
+            return _instance;
+        }
+
+        SqlConnection baglan = new SqlConnection("Server=.; Database=Northwind; UID=sa; PWD=123");
+        
+        public int Insert(string sqlCmd)
+        {
+            SqlCommand cmd = new SqlCommand(sqlCmd, baglan);
+            if (baglan.State == System.Data.ConnectionState.Closed)
+            {
+                baglan.Open();
+            }
+
+            int sonuc = cmd.ExecuteNonQuery();
+            baglan.Close();
+
+            return sonuc;
+        }
+
+        public DataTable GetData(string sqlCmd)
+        {
+            SqlDataAdapter dap = new SqlDataAdapter(sqlCmd, baglan);
+            DataTable dt = new DataTable();
+            dap.Fill(dt);
+
+            return dt;
+        }
+    }
+}
